a1d3150c25b8993824bb7788f6aa45db16078980
DEDY MEYGA SAPUTRA <119374110+metropoliz00@users.noreply.github.com>
2026-03-12 09:11:42 +0700

    simpaspen
    ok

---
+++ story_content/user.js
@@ -0,0 +1,119 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5m1LwPU6uNG":
+        Script1();
+        break;
+      case "6ntvnDKweNS":
+        Script2();
+        break;
+      case "5xYLymHQqpc":
+        Script3();
+        break;
+      case "68mqwIdFRHG":
+        Script4();
+        break;
+      case "6QuDJ7uv8nk":
+        Script5();
+        break;
+  }
+}
+
+function Script1()
+{
+  if (!window.bgMusic) {
+    window.bgMusic = new Audio("musik1.mp3");
+    window.bgMusic.loop = true;
+    window.bgMusic.volume = 0.1; // volume awal (0.0 - 1.0)
+    window.bgMusic.play();
+}
+}
+
+function Script2()
+{
+  if (!window.audioSystem) {
+
+    window.audioSystem = {
+        track: null,
+        volume: 0.1,
+        isMuted: false
+    };
+
+    window.startMusic = function() {
+
+        if (!window.audioSystem.track) {
+
+            var audio = new Audio("musik1.mp3");
+            audio.loop = true;
+            audio.volume = window.audioSystem.volume;
+            audio.preload = "auto";
+
+            audio.play().catch(function(e){
+                console.log("Autoplay blocked until user interaction");
+            });
+
+            window.audioSystem.track = audio;
+        }
+    };
+
+    window.fadeOutMusic = function() {
+        var track = window.audioSystem.track;
+        if (!track) return;
+
+        var fade = setInterval(function() {
+            if (track.volume > 0.05) {
+                track.volume -= 0.05;
+            } else {
+                clearInterval(fade);
+                track.pause();
+            }
+        }, 100);
+    };
+
+    window.fadeInMusic = function() {
+        var track = window.audioSystem.track;
+        if (!track) return;
+
+        track.play();
+
+        var fade = setInterval(function() {
+            if (track.volume < window.audioSystem.volume) {
+                track.volume += 0.05;
+            } else {
+                clearInterval(fade);
+            }
+        }, 100);
+    };
+
+    window.toggleMute = function() {
+        var track = window.audioSystem.track;
+        if (!track) return;
+
+        window.audioSystem.isMuted = !window.audioSystem.isMuted;
+        track.muted = window.audioSystem.isMuted;
+    };
+}
+}
+
+function Script3()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script4()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
+function Script5()
+{
+  if (window.bgMusic) {
+    window.bgMusic.pause();
+}
+}
+
